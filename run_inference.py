63ac68de07f14fa3317560f3570ad3653a471a41
In Kyu Lee <i243l699@kumc.edu>
2022-02-16 09:51:43 -0600

    [Update] lobe post process is added as an option

--- run_inference.py
+++ run_inference.py
@@ -12,6 +12,7 @@ from networks.ZUNet_v1 import ZUNet_v1
 from engine import *
 from dataloader import *
 from losses import *
+from postprocess_lobe import clean_up_lobe
 
 # ML
 from torch.cuda import amp
@@ -26,13 +27,15 @@ sitk.ProcessObject_SetGlobalWarningDisplay(False)
 parser = argparse.ArgumentParser(description='segmentor')
 parser.add_argument('--mask', default='lobe', type=str, help='[airway, vessels, lung, lobe]')
 parser.add_argument('--model', default='ZUNet', type=str, help='[UNet, ZUNet]')
+parser.add_argument('--pp', default=True, type=bool, help='[True, False]')
+
 parser.add_argument('--subj_path', default='', type=str, help='Subject path, ex) VIDA_*/24')
-# parser.add_argument('--in_file_path',
-#     default='D:/silicosis/data/TE_ProjSubjList.in',
-#     type=str,
-#     help='path to *.in')
+parser.add_argument('--in_file_path',
+    default='D:/silicosis/data/TE_ProjSubjList.in',
+    type=str,
+    help='path to *.in')
 parser.add_argument('--parameter_path',
-    default="RESULTS/airway/airway_UNet_n0_20220128/airway_UNet_n0_1.pth",
+    default="RESULTS\lobe\lobe_ZUNet_n0_20220127\lobe_ZUNet_n0_45.pth",
     type=str,
     help='path to *.pth')
 
@@ -42,7 +45,7 @@ def get_config():
     config = wandb.config
     # ENV
     config.data_path = os.getenv("VIDA_PATH")
-    # config.in_file_path = args.in_file_path
+    config.in_file_path = args.in_file_path
     config.subj_path = args.subj_path
     config.parameter_path = args.parameter_path
     config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -59,7 +62,8 @@ def get_config():
         config.Z = True
     else:
         config.Z = False
-    config.in_c = 1
+    config.pp = args.pp
+    config.in_c = 4
     
     return config
 
@@ -83,37 +87,36 @@ if __name__ == "__main__":
         model.to(config.device)
         eng = Segmentor(model=model, device=config.device)
     
-    # infer_df = pd.read_csv(config.in_file_path, sep='\t')
-    # pbar = tqdm(range(len(infer_df)))
-    # for i in pbar:
-        # subj_path = infer_df.ImgDir[i]
-    subj_path = config.subj_path
-    print(subj_path)
-    img_path = os.path.join(subj_path,'zunu_vida-ct.img')
-    if not os.path.exists(img_path):
-        DCMtoVidaCT(subj_path)
-    if config.in_c==1:
-        img, hdr = prep_test_img(img_path, multiC=False)
-        pred = eng.inference(img)
 
-    else:
-        img, hdr = prep_test_img(img_path, multiC=True)
-<<<<<<< HEAD
-        pred = eng.inference_multiC(img)
+    infer_df = pd.read_csv(config.in_file_path, sep='\t')
+    pbar = tqdm(range(len(infer_df)))
+    for i in pbar:
+        subj_path = infer_df.ImgDir[i]
+    # subj_path = config.subj_path
+        print(subj_path)
+        img_path = os.path.join(subj_path,'zunu_vida-ct.img')
+        if not os.path.exists(img_path):
+            DCMtoVidaCT(subj_path)
+        if config.in_c==1:
+            img, hdr = prep_test_img(img_path, multiC=False)
+            pred = eng.inference(img)
 
-    # img, hdr = load(img_path)
-    # img[img<-1024] = -1024
-    # img = (img - np.min(img)) / (np.max(img) - np.min(img))
-=======
-    pred = eng.inference_multiC(img,n_class=config.num_c)
->>>>>>> 362106be6963aa3ae12785a17d11c442b94af450
-    if config.mask == 'lobe':
-        pred[pred==1] = 8
-        pred[pred==2] = 16
-        pred[pred==3] = 32
-        pred[pred==4] = 64
-        pred[pred==5] = 128
-    elif config.mask == 'airway':
-        pred[pred==1] = 255
+        else:
+            img, hdr = prep_test_img(img_path, multiC=True)
+            pred = eng.inference_multiC(img)
 
-    save(pred,os.path.join(subj_path,f'{config.model}-{config.mask}.img.gz'),hdr=hdr)
+        if config.mask == 'lobe':
+            pred[pred==1] = 8
+            pred[pred==2] = 16
+            pred[pred==3] = 32
+            pred[pred==4] = 64
+            pred[pred==5] = 128
+        elif config.mask == 'airway':
+            pred[pred==1] = 255
+        save_path = os.path.join(subj_path,f'{config.model}_in_c{config.in_c}-{config.mask}.img.gz')
+        # print(f'save: {save_path}')
+        save(pred,save_path,hdr=hdr)
+        if config.pp:
+            save_path_pp = os.path.join(subj_path,f'{config.model}_in_c{config.in_c}-{config.mask}_pp.img.gz')
+            mask_pp = clean_up_lobe(pred)
+            save(mask_pp,save_path_pp,hdr=hdr)
