e11ac5733a4e6cfbffdc8a5f8c633c0e9a569923
In Kyu Lee <i243l699@kumc.edu>
2022-05-27 13:14:26 -0500

    [Fix] wandb config is removed

--- run_inference.py
+++ run_inference.py
@@ -41,9 +41,9 @@ parser.add_argument('--parameter_path',
     type=str,
     help='path to *.pth')
 
+args = parser.parse_args()
 
-class CFD:
-    
+class CFG:
     # ENV
     in_file_path = args.in_file_path
     subj_path = args.subj_path
@@ -70,36 +70,6 @@ class CFD:
         in_c = 1
 
     
-# def get_config(args):
-#     config = wandb.config
-#     # ENV
-#     config.in_file_path = args.in_file_path
-#     config.subj_path = args.subj_path
-#     config.parameter_path = args.parameter_path
-#     config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     config.mask = args.mask # 'airway', 'lung', 'lobes'
-#     config.model = args.model
-#     config.pp = args.pp
-#     if args.mask == 'lobes':
-#         config.num_c = 6
-#     elif args.mask == 'lung':
-#         config.num_c = 3
-#     else:
-#         config.num_c = 2
-    
-#     if config.model == 'ZUNet':
-#         config.Z = True
-#     else:
-#         config.Z = False
-
-#     if args.multi_channel:
-#         config.in_c = 4
-#     else:
-#         config.in_c = 1
-
-    
-#     return config
-
 def get_chest_mask_slice(img,kernelsize=3):
     # img: [512,512]
     img[img<-1024] = -1024
@@ -249,8 +219,7 @@ def main(config):
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    
     print(args)
-    # config = get_config(args)
-    config = CFD(args)
+    config = CFG
     main(config)
